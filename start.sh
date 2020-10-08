e5bcecaab5993922a3e984c585c44cb0e9b4f741
yaevvv <ievgen.iaremenko@gmail.com>
2020-10-08 11:06:58 +0300

    Updated data

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+######################
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+#######################
+docker-compose up -d
+#######################
+cp index.php ./nginx/var/www
+cp testdomain.com_nginx.crt ./nginx/conf
+cp testdomain.com_nginx.key ./nginx/conf
+cp ssl ./nginx/conf
+cp default.conf ./nginx/conf
+
